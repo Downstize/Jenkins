dac5b7eb74f6410f206c2c5842070adaa3a966b3
downstize <swankydid@gmail.com>
2024-12-17 00:15:49 +0300

    Init jenkins

---
+++ src/main/java/org/example/jenkins/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.example.jenkins.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World! My name is Slava and i'm turtle";
+    }
+}
